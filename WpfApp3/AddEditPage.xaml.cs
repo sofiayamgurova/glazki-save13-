b2d19b676f286ec4998bf99b29e136bba0734e36
User1 <User1@DESKTOP-V3JV86K>
2024-12-18 18:34:18 +0500

    Добавьте файлы проекта.

---
+++ WpfApp3/AddEditPage.xaml.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+
+    {
+        private Agent currentAgent;
+        public AddEditPage(Agent agent)
+        {
+            InitializeComponent();
+            currentAgent = agent ?? new Agent();
+            DataContext = currentAgent;
+
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(currentAgent.Title))
+                errors.AppendLine("Укажите наименование агента");
+            if (string.IsNullOrWhiteSpace(currentAgent.Address))
+                errors.AppendLine("Укажите адрес агента");
+            if (string.IsNullOrWhiteSpace(currentAgent.DirectorName))
+                errors.AppendLine("Укажите ФИО директора");
+            if (ComboType.SelectedItem == null)
+                errors.AppendLine("Укажите тип агента");
+            else
+                currentAgent.AgentTypeID = ComboType.SelectedIndex + 1;
+            if (string.IsNullOrWhiteSpace(currentAgent.Priority.ToString()))
+                errors.AppendLine("Укажите приоритет агента");
+            if (currentAgent.Priority <= 0)
+                errors.AppendLine("Укажите положительный приоритет агента");
+            if (string.IsNullOrWhiteSpace(currentAgent.INN))
+                errors.AppendLine("Укажите ИНН агента");
+            if (string.IsNullOrWhiteSpace(currentAgent.KPP))
+                errors.AppendLine("Укажите КПП агента");
+            if (string.IsNullOrWhiteSpace(currentAgent.Phone))
+                errors.AppendLine("Укажите телефон агента");
+            else
+            {
+                string ph = currentAgent.Phone.Replace("(", "").Replace("-", "").Replace("+", "");
+                if (((ph[1] == '9' || ph[1] == '4' || ph[1] == '8') && ph.Length != 11)
+                || (ph[1] == '3' && ph.Length != 12))
+                    errors.AppendLine("Укажите правильно телефон агента");
+            }
+            if (string.IsNullOrWhiteSpace(currentAgent.Email))
+                errors.AppendLine("Укажите почту агента");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (currentAgent.ID == 0)
+                YamgurovaGlazkiSaveEntities.GetContext().Agent.Add(currentAgent);
+            try
+            {
+                YamgurovaGlazkiSaveEntities.GetContext().SaveChanges();
+                MessageBox.Show("информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var _currentAgent = (sender as Button).DataContext as Agent;
+
+            var currentProductSale = YamgurovaGlazkiSaveEntities.GetContext().ProductSale.ToList();
+            currentProductSale = currentProductSale.Where(p => p.AgentID == _currentAgent.ID).ToList();
+
+            if (currentProductSale.Count != 0)
+                MessageBox.Show("Невозможно выполнить удаление, так как существует история реализации продуктов");
+            else
+            {
+                var currentAgentPriorityHistory = YamgurovaGlazkiSaveEntities.GetContext().AgentPriorityHistory.ToList();
+                var currentShop = YamgurovaGlazkiSaveEntities.GetContext().Shop.ToList();
+                currentAgentPriorityHistory = currentAgentPriorityHistory.Where(p => p.AgentID == _currentAgent.ID).ToList();
+                currentShop = currentShop.Where(p => p.AgentID == _currentAgent.ID).ToList();
+
+
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        YamgurovaGlazkiSaveEntities.GetContext().Agent.Remove(_currentAgent);
+
+                        if (currentAgentPriorityHistory.Count != 0)
+                        {
+                            for (int i = 0; currentAgentPriorityHistory.Count == i; i++)
+                                YamgurovaGlazkiSaveEntities.GetContext().AgentPriorityHistory.Remove(currentAgentPriorityHistory[i]);
+                        }
+                        if (currentShop.Count != 0)
+                        {
+                            for (int i = 0; currentShop.Count == i; i++)
+                                YamgurovaGlazkiSaveEntities.GetContext().Shop.Remove(currentShop[i]);
+                        }
+                        YamgurovaGlazkiSaveEntities.GetContext().SaveChanges();
+
+                        MessageBox.Show("Информация удалена!");
+                        Manager.MainFrame.GoBack();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+
+        }
+
+        private void ChangePictureBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myOpenFileDialog = new OpenFileDialog();
+            if (myOpenFileDialog.ShowDialog() == true)
+            {
+                currentAgent.Logo = myOpenFileDialog.FileName;
+                LogoImage.Source = new BitmapImage(new Uri(myOpenFileDialog.FileName));
+
+            }
+        }
+    }
+}
---
+++ WpfApp3/AgentPage.xaml.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentPage.xaml
+    /// </summary>
+    public partial class AgentPage : Page
+    {
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+
+        List<Agent> CurrentPageList = new List<Agent>();
+        List<Agent> TableList;
+        public AgentPage()
+        {
+            InitializeComponent();
+            var currentService = YamgurovaGlazkiSaveEntities.GetContext().Agent.ToList();
+            AgentListView.ItemsSource = currentService;
+
+            ComboType.SelectedIndex = 0;
+            ComboAgentType.SelectedIndex = 0;
+            UpdateAgents();
+
+        }
+
+        private void ChangePage(int direction, int? selectedPage)
+        {
+
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+
+            if (CountRecords % 10 > 0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+
+            Boolean Ifupdate = true;
+
+            int min;
+
+            if (selectedPage.HasValue)
+            {
+                if (selectedPage >= 0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage * 10; i < min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch (direction)
+                {
+                    case 1:
+
+                        if (CurrentPage > 0)
+                        {
+
+
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+
+                    case 2:
+                        if (CurrentPage < CountPage - 1)
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+
+                        break;
+
+                }
+            }
+
+            if (Ifupdate)
+            {
+                PageListBox.Items.Clear();
+                for (int i = 1; i <= CountPage; i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+
+                min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                TBCount.Text = min.ToString();
+                TBAllRecords.Text = " из " + CountRecords.ToString();
+                AgentListView.ItemsSource = CurrentPageList;
+                AgentListView.Items.Refresh();
+
+            }
+
+
+        }
+
+
+        private void UpdateAgents()
+        {
+            var currentAgents = YamgurovaGlazkiSaveEntities.GetContext().Agent.ToList();
+
+            currentAgents = currentAgents.Where(p => p.Title.ToLower().Contains(TBox_Search.Text.ToLower()) || p.Phone.Replace("-", " ").Replace("(", "").Replace(")", "").Replace(" ", "").Contains(TBox_Search.Text.ToLower()) || p.Email.ToLower().Contains(TBox_Search.Text.ToLower())).ToList();
+
+
+
+            currentAgents = currentAgents.ToList();
+
+            if (ComboAgentType.SelectedIndex == 1)
+            {
+                currentAgents = currentAgents.Where(p => p.AgentType.Title == "МФО").ToList();
+            }
+
+            if (ComboAgentType.SelectedIndex == 2)
+            {
+                currentAgents = currentAgents.Where(p => p.AgentType.Title == "ООО").ToList();
+            }
+
+            if (ComboAgentType.SelectedIndex == 3)
+            {
+                currentAgents = currentAgents.Where(p => p.AgentType.Title == "ЗАО").ToList();
+            }
+
+            if (ComboAgentType.SelectedIndex == 4)
+            {
+                currentAgents = currentAgents.Where(p => p.AgentType.Title == "МКК").ToList();
+            }
+
+            if (ComboAgentType.SelectedIndex == 5)
+            {
+                currentAgents = currentAgents.Where(p => p.AgentType.Title == "ОАО").ToList();
+            }
+
+            if (ComboAgentType.SelectedIndex == 6)
+            {
+                currentAgents = currentAgents.Where(p => p.AgentType.Title == "ПАО").ToList();
+            }
+
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentAgents = currentAgents.OrderBy(p => p.Title).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentAgents = currentAgents.OrderByDescending(p => p.Title).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentAgents = currentAgents.OrderBy(p => p.Priority).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentAgents = currentAgents.OrderByDescending(p => p.Priority).ToList();
+            }
+            if (ComboType.SelectedIndex == 5)
+            {
+                currentAgents = currentAgents.OrderBy(p => p. Discount).ToList();
+            }
+            if (ComboType.SelectedIndex == 6)
+            {
+                currentAgents = currentAgents.OrderByDescending(p => p.Discount).ToList();
+            }
+
+
+            AgentListView.ItemsSource = currentAgents.ToList();
+
+            TableList = currentAgents;
+
+            ChangePage(0, 0);
+
+        }
+       
+        private void TBox_Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+
+        private void ComboAgentType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Agent));
+
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
+        }
+   
+
+        private void Page_IsVisibleChanged_1(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                YamgurovaGlazkiSaveEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                AgentListView.ItemsSource = YamgurovaGlazkiSaveEntities.GetContext().Agent.ToList();
+            }
+            UpdateAgents();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+       
+        public SolidColorBrush FonStyle
+        {
+            get
+            {
+                if (Discount > 24)
+                {
+                    return (SolidColorBrush)new BrushConverter().ConvertFromString("LightGreen");
+
+                }
+                else
+                {
+                    return (SolidColorBrush)new BrushConverter().ConvertFromString("White");
+
+                }
+            }
+
+        }
+    }
+}
+    }
+    }
